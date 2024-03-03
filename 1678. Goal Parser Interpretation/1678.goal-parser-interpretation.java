f744bb3348c2da3b87f0429f7436ae2035a2c2fc
Emil <132345514+EmilWijayasekara@users.noreply.github.com>
2024-03-02 23:22:13 -0500

    Update 1678.goal-parser-interpretation.java

--- 1678. Goal Parser Interpretation/1678.goal-parser-interpretation.java
+++ 1678. Goal Parser Interpretation/1678.goal-parser-interpretation.java
@@ -2,7 +2,6 @@ class Solution {
     public String interpret(String command) {
         command = command.replace("()","o");
         command = command.replace("(al)","al");
-        // or we can do this in one line
         return command;
     }
 }
