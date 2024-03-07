0fd0d6af37d7d4d96f0827178a6db1c148c0815f
Emil <132345514+EmilWijayasekara@users.noreply.github.com>
2024-03-08 00:40:30 +0530

    Update 20.valid-parentheses.java

--- 20. Valid Parentheses/20.valid-parentheses.java
+++ 20. Valid Parentheses/20.valid-parentheses.java
@@ -1,10 +1,3 @@
-/*
- * @lc app=leetcode id=20 lang=java
- *
- * [20] Valid Parentheses
- */
-
-// @lc code=start
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
@@ -18,5 +11,3 @@ class Solution {
         return stack.isEmpty();
     }
 }
-// @lc code=end
-
